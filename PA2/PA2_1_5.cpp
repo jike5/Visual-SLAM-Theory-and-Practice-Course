8238f2b60c4bb7c1b21094cf21a23ffa3e5c7bd7
jike5 <2719825969@qq.com>
2021-08-03 16:52:24 +0800

    init

---
+++ PA2/PA2_1_5.cpp
@@ -0,0 +1,15 @@
+// PA2 第一题第5小问
+// 实现随机矩阵100*100的A的QR分解和Cholesky分解求解Ax=b
+#include<iostream>
+#include<Eigen/Core>
+#include<Eigen/Dengse>
+
+#define MATRIX_SIZE 50
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Eigen::Matrix<float, -1, -1> A;
+    
+    return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"hello.h"
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	sayHello();    
+	return 0;
+}
